f1ecc3f38c08d5543ebbdab0f4f498a3edf99799
wahasa <69626847+wahasa@users.noreply.github.com>
2024-08-17 11:50:28 +0700

    bootstrap.sh

--- BlackArch/bootstrap.sh
+++ BlackArch/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-pacman -S sudo curl
+pacman -S dialog nano sudo curl --noconfirm
 
 curl -O https://blackarch.org/strap.sh
+
 echo 26849980b35a42e6e192c6d9ed8c46f0d6d06047 strap.sh | sha1sum -c
 chmod +x strap.sh ; sudo ./strap.sh
 
