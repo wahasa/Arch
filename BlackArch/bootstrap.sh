c0b51bdfa973bf393d7b1cc44d7e8a326cdcc703
wahasa <69626847+wahasa@users.noreply.github.com>
2024-08-17 13:00:55 +0700

    bootstrap.sh

--- BlackArch/bootstrap.sh
+++ BlackArch/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-clear
+   clear
    echo ""
    echo "Installing BlackArch,."
    echo ""
+pacman -Syu
 
 #Prepare Packages
 pacman -S dialog nano --noconfirm
@@ -11,14 +12,28 @@ pacman -S sudo curl neofetch --noconfirm
 #Blackarch Packages
 curl -O https://blackarch.org/strap.sh
 echo 26849980b35a42e6e192c6d9ed8c46f0d6d06047 strap.sh | sha1sum -c
-chmod +x strap.sh ; sudo ./strap.sh
+chmod +x strap.sh ; ./strap.sh
 
 #Extra Packages
 rm -rf /etc/os-release
 wget -q https://raw.githubusercontent.com/wahasa/Arch/main/BlackArch/os-release /etc/
 
-sudo pacman -Syu
+pacman -Syu
   echo ""
   echo "installation completed"
   echo ""
-  rm strap.sh bootstrap.sh
+  clear
+  echo "Menu BlackArch Linux
+- List All Tools BlackArch, run
+pacman -Sgg | grep blackarch | cut -d' ' -f2 | sort -u
+- List Categories BlackArch, run
+pacman -Sg | grep blackarch
+- Install Tool BlackArch, run
+pacman -S <tool>
+- Install category BlackArch, run
+pacman -S blackarch-<category>
+
+You can see again, run 'cat Note'" > ~/Note
+
+cat ~/Note
+rm strap.sh bootstrap.sh
